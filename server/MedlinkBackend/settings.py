3f3a50d16dcdc731d7b5c2b18ff2edd0591d9cd9
gilbertketer <gilbertketer759@gmail.com>
2024-10-08 16:05:34 +0300

    New updates

--- server/MedlinkBackend/settings.py
+++ server/MedlinkBackend/settings.py
@@ -63,7 +63,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 # Application definition
