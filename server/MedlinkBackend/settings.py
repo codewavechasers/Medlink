9dc9bdf78a8d599647bb0a343788d927241934dc
gilbertketer <gilbertketer759@gmail.com>
2024-10-09 22:39:15 +0300

    changes

--- server/MedlinkBackend/settings.py
+++ server/MedlinkBackend/settings.py
@@ -10,8 +10,8 @@ ENCRYPTION_KEY = env('ENCRYPTION_KEY')
 
 # Ensure you add other settings like DATABASES, INSTALLED_APPS, etc.
 #celery
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
