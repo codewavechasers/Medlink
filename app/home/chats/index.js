7225e043e9b5786d4accd5f2c2774f2a7222437b
gilbertketer <gilbertketer759@gmail.com>
2024-10-10 14:06:24 +0300

    changes

--- app/home/chats/index.js
+++ app/home/chats/index.js
@@ -1,30 +1,13 @@
 import React, { useEffect } from "react";
 import "./styles.scss";
-import Features from "../../../components/Features";
-import Intergrations from "../../../components/intergrations";
-import TechTrust from "../../../components/Tech-trust";
-import MoreEnquiry from "../../../components/more-inquiry";
-import WithAuthRedirect from "@/app/api/withAuthRedirect.js";
 
 function Chats() {
   useEffect(() => {
-    const hasRefreshed = localStorage.getItem("hasRefreshed");
-
-    // if (!hasRefreshed) {
-    //   localStorage.setItem("hasRefreshed", "true");
-    //   localStorage.setItem("selectedItem", "chats");
-    //   window.location.reload();
-    // } else {
-    //   localStorage.removeItem("hasRefreshed");
-    // }
-  }, []);
-
-  useEffect(() => {
-    const customElement = document.querySelector('#WebChatContainer');
+    const customElement = document.querySelector("#WebChatContainer");
 
     window.watsonAssistantChatOptions = {
       integrationID: "c60710e8-ce57-4c1a-a1af-a3248da21f48",
-      region: "us-south", 
+      region: "us-south",
       serviceInstanceID: "3e1f34c5-2bff-4a50-ace2-6d6a6f7ec59e",
       element: customElement,
       onLoad: async (instance) => {
@@ -45,27 +28,18 @@ function Chats() {
   }, []);
 
   const chatContainerStyle = {
-    width: '100%',
-    height: '90vh',
-    // zIndex: 1000,  
-    backgroundImage: 'url("/chat-images/bg.png")', 
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat' 
+    width: "100%",
+    height: "90vh",
+    backgroundImage: 'url("/chat-images/bg.png")',
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
   };
 
   return (
-    // <WithAuthRedirect>
     <div className="chat-cont">
-      <div id="WebChatContainer" style={chatContainerStyle}>
-        
-      </div>
-      <Features />
-      <TechTrust />
-      <Intergrations />
-      <MoreEnquiry />
+      <div id="WebChatContainer" style={chatContainerStyle}></div>
     </div>
-    // </WithAuthRedirect>
   );
 }
 
