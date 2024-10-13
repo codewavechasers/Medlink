55890eaeaa226a4d1cb0f79400464216819bf268
gilbertketer <gilbertketer759@gmail.com>
2024-10-13 19:41:04 +0300

    changes

--- app/home/chats/index.js
+++ app/home/chats/index.js
@@ -4,19 +4,14 @@ import ChatInterface from "@/components/chat";
 function Chats() {
   useEffect(() => {
     window.watsonAssistantChatOptions = {
-      integrationID: "4be8d9d0-5f37-4c69-8127-fd9df29f2d02", // The ID of this integration.
+      integrationID: "48155efb-fe29-4abd-bda9-59fb57ce2423", // The ID of this integration.
       region: "us-south", // The region your integration is hosted in.
       serviceInstanceID: "acaca65e-f26d-4827-9ccb-9fbecfc1fdad", // The ID of your service instance.
-      onLoad: async (instance) => {
-        await instance.render();
-      },
+      onLoad: async (instance) => { await instance.render(); }
     };
-    setTimeout(function () {
-      const t = document.createElement("script");
-      t.src =
-        "https://web-chat.global.assistant.watson.appdomain.cloud/versions/" +
-        (window.watsonAssistantChatOptions.clientVersion || "latest") +
-        "/WatsonAssistantChatEntry.js";
+    setTimeout(function(){
+      const t=document.createElement('script');
+      t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
       document.head.appendChild(t);
     });
   }, []);
