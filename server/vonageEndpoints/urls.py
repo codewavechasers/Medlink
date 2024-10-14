a51e678d914b494ecbfc3c683660232cc9c56a6f
gilbertketer <gilbertketer759@gmail.com>
2024-10-14 16:39:15 +0300

    Server updates

--- server/vonageEndpoints/urls.py
+++ server/vonageEndpoints/urls.py
@@ -2,8 +2,8 @@
 from .views import event_url, answer_url, fallback_url
 from django.urls import path
 urlpatterns = [
-    path('/fallback/', fallback_url, name='fallback_url'),
-    path('/event/', event_url, name='event_url'),
-    path('/answer/', answer_url, name='answer_url'),
+    path('fallback/', fallback_url, name='fallback_url'),
+    path('event/', event_url, name='event_url'),
+    path('answer/', answer_url, name='answer_url'),
 
 ]
